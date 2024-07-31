88a9c79acec85e89f34f9c1125ac65162d22a255
SantiGall <164009868+SantiGall@users.noreply.github.com>
2024-07-31 15:59:28 -0300

    Jugador ahora mira hacia donde se mueve

--- Rampage Arena/Assets/Scripts/ThirdPersonMovement.cs
+++ Rampage Arena/Assets/Scripts/ThirdPersonMovement.cs
@@ -6,6 +6,8 @@ public class ThirdPersonMovement : MonoBehaviour
 {
     public CharacterController controller;
     public float speed;
+    public float turnSmoothTime;
+    float turnSmoothVelocity;
 
     void Update()
     {
@@ -15,6 +17,10 @@ public class ThirdPersonMovement : MonoBehaviour
 
         if (direction.magnitude >= 0.1f)
         {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
             controller.Move(direction * speed * Time.deltaTime);
         }
     }
