54ee8bb6572220c2cc8d37900aa5f3f763a8039c
SantiGall <164009868+SantiGall@users.noreply.github.com>
2024-08-02 10:41:38 -0300

    variable de porcentaje agregada

---
+++ Rampage Arena/Assets/Scripts/HealthManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthManager : MonoBehaviour
+{
+    public float percen = 0;
+
+    void Update()
+    {
+        
+    }
+}
