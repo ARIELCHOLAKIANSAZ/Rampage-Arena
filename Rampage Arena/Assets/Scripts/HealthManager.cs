a15eed69b950715e1505a4f3e457ca61f4e23e16
SantiGall <164009868+SantiGall@users.noreply.github.com>
2024-08-08 09:03:39 -0300

    controlador del display del porcentaje agregadas

--- Rampage Arena/Assets/Scripts/HealthManager.cs
+++ Rampage Arena/Assets/Scripts/HealthManager.cs
@@ -5,9 +5,15 @@ using UnityEngine;
 public class HealthManager : MonoBehaviour
 {
     public float percen = 0;
+    float percenLast = 0;
+    public PercentageDisplayManager display;
+    public int guy;
 
     void Update()
     {
-        
+        if (percenLast != percen)
+        {
+            display.PercenUpdate(percen, guy);
+        }
     }
 }
---
+++ Rampage Arena/Assets/Scripts/PercentageDisplayManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PercentageDisplayManager : MonoBehaviour
+{
+    public Text[] percenDisplay;
+
+    public void PercenUpdate(float Percentage, int num)
+    {
+        percenDisplay[num].text = Percentage.ToString() + "%";
+    }
+
+}
