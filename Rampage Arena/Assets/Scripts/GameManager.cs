2bb3dd05d8e54ae7af2ecf1abfc332c00e3c2572
SantiGall <164009868+SantiGall@users.noreply.github.com>
2024-08-21 15:54:56 -0300

    GameManager funciona

--- Rampage Arena/Assets/Scripts/GameManager.cs
+++ Rampage Arena/Assets/Scripts/GameManager.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static GameManager Instance;
+    void Awake()
     {
-        
+        if(Instance != this && Instance != null)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;
+        }
+
+        DontDestroyOnLoad(this);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.anyKeyDown && SceneManager.GetActiveScene().name == "Menu") ChangeScene("RoomMenu");
+    }
+
+    public void ChangeScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
     }
 }
