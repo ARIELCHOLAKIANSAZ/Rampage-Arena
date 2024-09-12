06106f21edb5e969cf310478b069abb5ec1e5ce1
SantiGall <164009868+SantiGall@users.noreply.github.com>
2024-09-12 10:04:36 -0300

    Countdown agregada

--- Rampage Arena/Assets/Scripts/BattleManager.cs
+++ Rampage Arena/Assets/Scripts/BattleManager.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using Alteruna;
+using UnityEngine.UI;
 
 public class BattleManager : AttributesSync
 {
-        public Vector3 sp = new Vector3 (0, 0, 0);
+    public Vector3 sp = new Vector3 (0, 0, 0);
+    public Text count;
     public void Start()
     {  
         int p = GameObject.Find("PLAYERMANAGER").GetComponent<PlayerManager>().playerNumber;
@@ -26,11 +28,44 @@ public class BattleManager : AttributesSync
         {
             sp = GameObject.Find("SPAWNPOINT4").transform.position;
         }
-            Multiplayer.SpawnAvatar(sp);
+        Multiplayer.SpawnAvatar(sp);
+        StartCoroutine(countDown());
     }
 
-    void Update()
+
+    IEnumerator countDown()
     {
-        
+        count.text = "3";
+        Alteruna.Avatar[] gaobjs = FindObjectsOfType<Alteruna.Avatar>();
+        ThirdPersonMovement[] tpm = { null, null, null, null };
+        for (int i = 0; i < gaobjs.Length; i++)
+        {
+            tpm[i] = gaobjs[i].gameObject.GetComponent<ThirdPersonMovement>();
+        }
+        AttacksManager[] am = { null, null, null, null };
+        for (int i = 0; i < gaobjs.Length; i++)
+        {
+            am[i] = gaobjs[i].gameObject.GetComponent<AttacksManager>();
+        }
+        for (int i = 0; i < gaobjs.Length; i++)
+        {
+            am[i].lcked = true;
+            tpm[i].locked = true;
+            tpm[i].jlock = true;
+        }
+        yield return new WaitForSeconds(1);
+        count.text = "2";
+        yield return new WaitForSeconds(1);
+        count.text = "1";
+        yield return new WaitForSeconds(1);
+        for (int i = 0; i < gaobjs.Length; i++)
+        {
+            am[i].lcked = false;
+            tpm[i].locked = false;
+            tpm[i].jlock = false;
+        }
+        count.text = "RAMPAGE";
+        yield return new WaitForSeconds(1);
+        count.text = "";
     }
 }
