a3b34161e4d6dbf0c1013347a72a20ae806df824
SantiGall <164009868+SantiGall@users.noreply.github.com>
2024-09-19 10:19:10 -0300

    Killzone Works

--- Rampage Arena/Assets/Scripts/DeathManager.cs
+++ Rampage Arena/Assets/Scripts/DeathManager.cs
@@ -5,20 +5,37 @@ using Alteruna;
 
 public class DeathManager : MonoBehaviour
 {
-    int lives = 3; 
+    int lives = 3;
+    bool done = false;
     private void OnTriggerExit(Collider other)
     {
         Debug.Log("left");
         Alteruna.Avatar ava = other.GetComponent<Alteruna.Avatar>();
         if (!ava.IsMe) return;
         Debug.Log("avaisme");
-        ava.gameObject.transform.position = GameObject.Find("SPAWNPOINT1").transform.position;
-        Debug.Log("positioned");
-        lives -= 1;
-        Debug.Log(lives.ToString());
+        ThirdPersonMovement tpm = other.GetComponent<ThirdPersonMovement>();
+        tpm.gravAffect = false;
+        tpm.locked = true;
+        tpm.jlock = true;
+        StartCoroutine(timer(other, tpm));
         if (lives <= 0)
         {
-            Destroy(ava.gameObject);
+            Destroy(other.gameObject);
         }
+    }
+    IEnumerator timer(Collider other, ThirdPersonMovement tpm)
+    {
+        yield return new WaitForSeconds(0.1f);
+        other.transform.position = GameObject.Find("SPAWNPOINT1").transform.position;
+        Debug.Log(other.transform.position.ToString() + " positioned to " + GameObject.Find("SPAWNPOINT1").transform.position);
+        yield return new WaitForSeconds(0.05f);
+        other.transform.position = GameObject.Find("SPAWNPOINT1").transform.position;
+        Debug.Log(other.transform.position.ToString() + " positioned to " + GameObject.Find("SPAWNPOINT1").transform.position);
+        yield return new WaitForSeconds(0.1f);
+        lives -= 1;
+        tpm.gravAffect = true;
+        tpm.locked = false;
+        tpm.jlock = false;
+        Debug.Log(lives.ToString());
     }
 }
