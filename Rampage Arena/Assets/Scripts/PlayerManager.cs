c893e179eda2698f19c1ca0ee9442fd7381825bf
SantiGall <164009868+SantiGall@users.noreply.github.com>
2024-11-27 10:34:12 -0300

    Retoques Finales

--- Rampage Arena/Assets/Scripts/PlayerManager.cs
+++ Rampage Arena/Assets/Scripts/PlayerManager.cs
@@ -9,9 +9,9 @@ using UnityEngine.SceneManagement;
 public class PlayerManager : AttributesSync
 {
     public int playerNumber;
-    public Alteruna.Avatar chosenCharacter;
+    public GameObject chosenCharacter;
     public int[] chosenCharacters = { 3, 3, 3, 3 };
-    public Alteruna.Avatar[] charList;
+    public GameObject[] charList;
     public GameObject[] btnList;
     public Text[] charName;
     public bool[] activePlayerList = { false, false, false, false };
@@ -85,7 +85,6 @@ public class PlayerManager : AttributesSync
         chosenCharacter = charList[num];
         pnumM1 = playerNumber - 1;
         BroadcastRemoteMethod("setChosenChars", pnumM1, num);
-        GameObject.Find("NETWORK MANAGER").GetComponent<Alteruna.Multiplayer>().AvatarPrefab = chosenCharacter;
     }
 
     public void ChooseNumber(int num)
